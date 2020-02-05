20e5e18f64c2b55a42cd4b8b39437fd8194575f2
arnab_mohit <arnab_mohit>
2020-02-05 17:09:04 +0600

    sdfsd

---
+++ src/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <app-service v-for="i in 5" :key="i" ></app-service>
+  </div>
+</template>
+
+<script>
+import AppService from './AppService.vue';
+
+  export default{
+    components:{
+      'app-service': AppService
+    }
+
+  }
+</script>
